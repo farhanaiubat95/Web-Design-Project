9666a7e4216d294aa6a6c4b2d494099ae50efa5a
Farhana Bente Islam <21303195@iubat.edu>
2024-01-14 22:41:12 +0600

    First time

---
+++ 9.Veer/js/custom.js
@@ -0,0 +1,47 @@
+
+new WOW().init();
+
+
+$('.slide1').slick({
+    dots: true,
+    infinite: true,
+    speed: 3000,
+    autoplay:true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.slide2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
